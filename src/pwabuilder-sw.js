856074340bb569b4b0a23006247d1f55edb82790
olafsulich <olafsulich@gmail.com>
2020-01-18 06:47:17 +0100

    Change start_ulr in manifest.json

--- src/pwabuilder-sw.js
+++ src/pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = 'pwabuilder-offline';
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
-const offlineFallbackPage = '/public/index.html';
+const offlineFallbackPage = '/public/offline.html';
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function(event) {
@@ -13,7 +13,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE).then(function(cache) {
       console.log('[PWA Builder] Cached offline page during install');
 
-      if (offlineFallbackPage === '/public/index.html') {
+      if (offlineFallbackPage === '/public/offline.html') {
         return cache.add(
           new Response(
             'TODO: Update the value of the offlineFallbackPage constant in the serviceworker.',
